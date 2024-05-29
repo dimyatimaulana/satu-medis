95c5a3a54c7df2fe13338e841528a7f9150a7bc2
Maulana Dimyati <dimyatimaulana@gmail.com>
2024-05-29 15:19:37 +0700

    feat: data search and pagination

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,27 +14,14 @@ const Navbar = ({ name }) => {
     }
   };
   return (
-    <div className="navbar bg-base-100 top-0 sticky shadow-md">
+    <div className="navbar bg-base-100 top-0 sticky shadow-md z-10">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">Satu Medis</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <a href="/dashboard">Dashboard</a>
           </li>
           <li>
             <div><FaUser /> {name}</div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import Navbar from "../components/Navbar";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import ReactPaginate from "react-paginate";
+import { FaSearch } from "react-icons/fa";
 
 const Dashboard = () => {
   const [user, setUser] = useState("");
@@ -16,6 +19,8 @@ const Dashboard = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getRefreshToken();
@@ -95,108 +100,175 @@ const Dashboard = () => {
     }
   };
 
+  function Users({ currentItems }) {
+    return (
+      <>
+        {currentItems &&
+          currentItems
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.name.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((user, idx) => (
+              <tr key={idx}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td className="flex gap-4">
+                  <button
+                    className="btn"
+                    onClick={() => {
+                      document.getElementById("editUser").showModal();
+                      setUserId(user.id);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <dialog id="editUser" className="modal">
+                    <div className="modal-box">
+                      <h3 className="font-bold text-lg">Edit User</h3>
+                      <form
+                        className="flex flex-col items-center"
+                      >
+                        <div className="w-full max-w-xs">
+                          <label
+                            htmlFor="name"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Name
+                          </label>
+                          <input
+                            type="text"
+                            className="input input-bordered w-full max-w-xs mb-3"
+                            placeholder="John Doe"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                          <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Email
+                          </label>
+                          <input
+                            type="email"
+                            className="input input-bordered w-full max-w-xs mb-3"
+                            placeholder="johndoe@company.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Password
+                          </label>
+                          <input
+                            type="password"
+                            className="input input-bordered w-full max-w-xs mb-3"
+                            placeholder="•••••••••"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+                        <button
+                          type="button"
+                          onClick={() => editUser(userId)}
+                          className="bg-primary w-full text-white max-w-xs"
+                        >
+                          Submit
+                        </button>
+                      </form>
+                    </div>
+                    <form method="dialog" className="modal-backdrop">
+                      <button className="bg-transparent">close</button>
+                    </form>
+                  </dialog>
+                  <button
+                    className="btn bg-red-500 text-white"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+      </>
+    );
+  }
+
+  function PaginatedItems({ itemsPerPage }) {
+    const [itemOffset, setItemOffset] = useState(0);
+    const endOffset = itemOffset + itemsPerPage;
+    // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = users.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(users.length / itemsPerPage);
+
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % users.length;
+      // console.log(
+      //   `User requested page number ${event.selected}, which is offset ${newOffset}`
+      // );
+      setItemOffset(newOffset);
+    };
+
+    return (
+      <>
+        <Users currentItems={currentItems} />
+        <tr>
+          <td></td>
+          <td></td>
+          <td>
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel=">"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="<"
+              renderOnZeroPageCount={null}
+              className="flex mt-4 justify-center gap-4 text-center"
+              pageClassName="rounded-[5px] text-base flex items-center justify-center"
+              pageLinkClassName="py-3 px-4"
+              activeClassName="bg-primary"
+              activeLinkClassName="text-white"
+              previousClassName="rounded-[5px] flex items-center justify-center"
+              previousLinkClassName="px-4 py-3 text-base"
+              nextClassName="rounded-[5px] flex items-center justify-center"
+              nextLinkClassName="px-4 py-3 text-base"
+            />
+          </td>
+        </tr>
+      </>
+    );
+  }
+
   return (
     <>
       <Navbar name={user} />
       <Toaster position="bottom-right" reverseOrder={false} />
       <div className="h-screen pt-5 px-8">
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto flex flex-col items-center">
+          <div className="input input-bordered flex items-center gap-2 mb-5">
+            <input
+              type="text"
+              placeholder="Name"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <FaSearch className="w-4 h-4 opacity-70" />
+          </div>
           <table className="table max-w-screen-lg">
             <thead>
               <tr>
-                <th>No</th>
+                <th>Id</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Options</th>
               </tr>
             </thead>
             <tbody>
-              {users &&
-                users.map((user, idx) => (
-                  <tr key={user.id}>
-                    <td>{idx + 1}</td>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td className="flex gap-4">
-                      <button
-                        className="btn"
-                        onClick={() => {
-                          document.getElementById("editUser").showModal();
-                          setUserId(user.id);
-                        }}
-                      >
-                        Edit
-                      </button>
-                      <dialog id="editUser" className="modal">
-                        <div className="modal-box">
-                          <h3 className="font-bold text-lg">Edit User</h3>
-                          <form
-                            action="submit"
-                            className="flex flex-col items-center"
-                          >
-                            <div className="w-full max-w-xs">
-                              <label
-                                htmlFor="name"
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                              >
-                                Name
-                              </label>
-                              <input
-                                type="text"
-                                className="input input-bordered w-full max-w-xs mb-3"
-                                placeholder="John Doe"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                              />
-                              <label
-                                htmlFor="email"
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                              >
-                                Email
-                              </label>
-                              <input
-                                type="email"
-                                className="input input-bordered w-full max-w-xs mb-3"
-                                placeholder="johndoe@company.com"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                              />
-                              <label
-                                htmlFor="password"
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                              >
-                                Password
-                              </label>
-                              <input
-                                type="password"
-                                className="input input-bordered w-full max-w-xs mb-3"
-                                placeholder="•••••••••"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                              />
-                            </div>
-                            <button
-                              type="button"
-                              onClick={() => editUser(userId)}
-                              className="bg-primary w-full text-white max-w-xs"
-                            >
-                              Submit
-                            </button>
-                          </form>
-                        </div>
-                        <form method="dialog" className="modal-backdrop">
-                          <button className="bg-transparent">close</button>
-                        </form>
-                      </dialog>
-                      <button
-                        className="btn bg-red-500 text-white"
-                        onClick={() => deleteUser(user.id)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
+              <PaginatedItems itemsPerPage={5} />
             </tbody>
           </table>
         </div>
