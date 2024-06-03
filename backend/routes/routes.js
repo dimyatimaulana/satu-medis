34cc93871829074ad68a997fc931a3147b65bb3e
Maulana Dimyati <dimyatimaulana@gmail.com>
2024-06-03 13:17:15 +0700

    feat: get one product & customer

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getUsers, register, login, logout, updateUser, deleteUser } from "../controllers/UserController.js";
-import { getProducts, createProducts, updateProduct, deleteProduct } from "../controllers/ProductController.js";
-import { getCustomers, createCustomer, updateCustomer, deleteCustomer } from "../controllers/CustomersController.js";
+import { getProducts, getOneProduct, createProducts, updateProduct, deleteProduct } from "../controllers/ProductController.js";
+import { getCustomers, getOneCustomer, createCustomer, updateCustomer, deleteCustomer } from "../controllers/CustomersController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
@@ -16,11 +16,13 @@ router.patch("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 
 router.get("/products", getProducts);
+router.get("/products/:id", getOneProduct);
 router.post("/products", createProducts);
 router.patch("/products/:id", updateProduct);
 router.delete("/products/:id", deleteProduct);
 
 router.get("/customers", getCustomers);
+router.get("/customers/:id", getOneCustomer);
 router.post("/customers", createCustomer);
 router.patch("/customers/:id", updateCustomer);
 router.delete("/customers/:id", deleteCustomer);
